3921b3e46dda593bed83c514aad9e582732e1440
GamboaHMA <manuelgamboahernandez35@gmail.com>
2025-03-19 00:23:30 -0400

    iniciando

---
+++ model/HexModel.py
@@ -0,0 +1,47 @@
+class HexModel:
+    def __init__(self, size):
+        self.size = size
+        self.board = [[None for _ in range(size)] for _ in range(size)]
+        self.current_player = 'Red'
+
+    # Por el momento solo dos jugadores, una casilla puede ser 0, 1 o -1, 0 casilla vacia, 1 jugador 1, -1 jugador 2
+    # Si vemos el tablero como un cuadrado, los bordes del jugador 1 seran izqu y derecha, jugador 2 arriba y abajo
+
+    def make_move(self, coord):
+        i = coord[0] 
+        j = coord[1]
+        if self.board == None:
+            self.board[i][j] = self.current_player
+            self.current_player = 'Blue' if self.current_player == 'Red' else 'Red'
+            return True
+        else:
+            return False
+        
+
+    def get_possible_movements(self):
+        possible_movements = []
+        
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.board[i][j] == None:
+                    possible_movements.append((i,j))
+            
+        return possible_movements
+
+    def game_over(self):
+
+        # todavia, esta mal
+        fila_1 = self.board[0]
+        fila_size = self.board[self.size - 1]
+
+        columna_1 = [fila[0] for fila in self.board]
+        columna_size = [fila[self.size - 1] for fila in self.board]
+
+        if 1 in columna_1 and 1 in columna_size:
+            return True
+        elif -1 in fila_1 and -1 in fila_size:
+            return True
+        else:
+            return False
+
+            
---
+++ view/HexView.py
@@ -0,0 +1,80 @@
+import pygame
+import math
+from model import HexModel
+
+class HexView:
+    def __init__(self, model:HexModel.HexModel, screen_size=600):
+        self.model = model
+        self.screen_size = screen_size
+        self.cell_size = screen_size // model.size
+        pygame.init()
+        self.screen = pygame.display.set_mode((screen_size, screen_size))
+        self.colors = {'Red': (255, 0, 0), 'Blue': (0, 0, 255)}
+
+    def draw_board(self):
+        self.screen.fill((255, 255, 255))
+        for row in range(self.model.size):
+            for col in range(self.model.size):
+                x = col * self.cell_size + self.cell_size // 2
+                y = row * self.cell_size + self.cell_size // 2
+                vertices = get_hex_vertices((x,y), self.cell_size // 2)
+
+                pygame.draw.polygon(self.screen, (0,0,0), vertices, 1)
+
+                if self.model.board[row][col]:
+                    ratio = self.cell_size // 2 - self.cell_size // 10
+                    pygame.draw.polygon(self.screen, (self.colors[self.model.board[row][col]]), ratio, 0)
+
+        pygame.display.flip()
+
+    def handle_click(self, pos):
+        row = pos[1] // self.cell_size
+        col = pos[0] // self.cell_size
+        if self.model.make_move((row, col)):
+            self.draw_board()
+            if self.model.game_over():
+                print(f"jugador {self.model.current_player} ha ganado")
+                pygame.quit()
+                exit()
+
+    def run(self):
+        running = True
+        self.draw_board()
+        while(running):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    self.handle_click(pygame.mouse.get_pos())
+        pygame.quit()
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def get_hex_vertices(center, ratio):
+    x, y = center
+    vertices = []
+    for i in range(6):
+        angle = 2 * math.pi / 6*i
+        vertice_x = x + ratio * math.cos(angle)
+        vertice_y = y + ratio * math.sin(angle)
+        vertices.append((vertice_x, vertice_y))
+    return vertices
